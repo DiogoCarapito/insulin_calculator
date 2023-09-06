77f230cdaf78824181198940cb211df33567a611
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-09-06 09:12:05 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title("Calculadora Insulina")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+
---
+++ utils/utils.py
@@ -0,0 +1,2 @@
+def cal_insulin():
+    pass
