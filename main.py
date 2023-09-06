86273089dcd29a6cab2be8d394d70363aa36e321
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-09-06 09:31:23 +0100

    initial commit

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import streamlit as st
 def main():
     st.title("Calculadora Insulina")
 
+    col_1, col_2, col_3 = st.columns(3)
+
+    with col_1:
+        st.session_state["glicemia"] = st.radio("Glicemia", ["estável", "ascendente", "descendente"])
+    with col_2:
+        st.session_state["alimento"] = st.selectbox("Alimento", ["maçã", "banana", "laranja"])
+    with col_3:
+        st.session_state["quantidade"] = st.number_input("Quantidade (g)", value=50, step=1)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
