c81b7faa72a12d4c57c6ef7c41079bd6c3936c4f
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-09-12 14:51:12 +0100

    insulin calc

--- main.py
+++ main.py
@@ -23,17 +23,29 @@ def load_data():
     return pd.read_csv("data/insa_tca_processed.csv")
 
 def formula_insulina(equivalencia_hc_insulina, fator_sensibilidade, alimento):
-    return 5
+
+    df_insa = load_data()
+
+    hc_por_100 = df_insa[df_insa["alimento_hc"] == alimento["Alimento"]][df_insa.columns[13]].values[0]
+
+    hc = alimento["Quantidade"]*hc_por_100/100
+
+    return hc
 
 
-def correcao_glicemia(fator_sensibilidade, glicemia, tendencia_glicemia):
-    return 10
+def correcao_glicemia(fator_sensibilidade, glicemia, glicemia_alvo):
+    return (glicemia - glicemia_alvo) / fator_sensibilidade
 
 
-def calculo_insulina(equivalencia_hc_insulina, fator_sensibilidade, glicemia, tendencia_glicemia, lista_alimentos):
+def calculo_insulina(equivalencia_hc_insulina, fator_sensibilidade, glicemia, glicemia_alvo, tendencia_glicemia, lista_alimentos):
 
     # insulin dose reset
     dose_insulina = 0
+
+    if glicemia < 60:
+        return 0
+
+    dose_insulina += correcao_glicemia(fator_sensibilidade, glicemia, glicemia_alvo)
 
     # check if there are foods in the list
     if len(lista_alimentos) != 0:
@@ -43,7 +55,15 @@ def calculo_insulina(equivalencia_hc_insulina, fator_sensibilidade, glicemia, te
             dose_insulina += formula_insulina(equivalencia_hc_insulina, fator_sensibilidade, alimento)
 
     # add the correction dose according to the glycemia and the trend
-    dose_insulina += correcao_glicemia(fator_sensibilidade, glicemia, tendencia_glicemia)
+
+    if tendencia_glicemia == "ascendete inclinado":
+        dose_insulina *= 1.2
+    elif tendencia_glicemia == "ascendente":
+        dose_insulina *= 1.1
+    elif tendencia_glicemia == "descendente inclinado":
+        dose_insulina *= 0.8
+    elif tendencia_glicemia == "descendente":
+        dose_insulina *= 0.9
 
     return dose_insulina
 
@@ -59,12 +79,13 @@ def main():
 
     df_insa = load_data()
 
+
     # lista de alimentos
     lista_alimentos = df_insa["alimento_hc"]
 
     #st.table(df_insa[["Nome do alimento","Hidratos de carbono [g]"]].head(10))
 
-    col_1, col_2, col_3, col_4 = st.columns(4)
+    col_1, col_2, col_3, col_4, col_5 = st.columns(5)
 
     with col_1:
         st.session_state["equivalencia_hc_insulina"] = st.number_input("Equivalencia HC", value=12, step=1, min_value=10, max_value=15)
@@ -73,31 +94,38 @@ def main():
     with col_3:
         st.session_state['glicemia'] = st.number_input("Glicémia (mg/dl)", value=100, step=5)
     with col_4:
-        st.session_state["tendencia_glicemia"] = st.radio("Tendencia Glicemia", ["estável", "ascendente", "descendente"], horizontal=True)
+        st.session_state["glicemia_alvo"] = st.number_input("Glicémia Alvo (mg/dl)", value=100, step=5)
+    with col_5:
+        st.session_state["tendencia_glicemia"] = st.radio("Tendencia Glicemia", ["estável", "ascendente", "ascendete inclinado", "descendente", "descendente inclinado"], horizontal=True)
 
     st.divider()
 
-    col_5, col_6= st.columns(2)
+    tab_1, tab_2 = st.tabs(["Lista de alimentos", "outros"])
+    with tab_1:
+        col_5, col_6= st.columns(2)
 
-    with col_5:
-        st.session_state["alimento"] = st.selectbox("Alimento", lista_alimentos)
-    with col_6:
-        st.session_state["quantidade"] = st.number_input("Quantidade (g)", value=50, step=5)
+        with col_5:
+            st.session_state["alimento"] = st.selectbox("Alimento", lista_alimentos, index=0)
+        with col_6:
+            st.session_state["quantidade"] = st.number_input("Quantidade (g)", value=50, step=5)
 
-    if st.button("adicionar alimento", type="secondary"):
-        st.session_state["lista_alimentos"].append(
-            {
-                "Alimento": st.session_state["alimento"],
-                "Quantidade":st.session_state["quantidade"]
-            }
-        )
+        if st.button("adicionar alimento", type="secondary"):
+            st.session_state["lista_alimentos"].append(
+                {
+                    "Alimento": st.session_state["alimento"],
+                    "Quantidade":st.session_state["quantidade"]
+                }
+            )
 
-    if st.button("limpar lista", type="primary"):
-        st.session_state["lista_alimentos"] = []
+        if st.button("limpar lista", type="primary"):
+            st.session_state["lista_alimentos"] = []
 
-    st.write("Lista de alimentos")
-    #st.write(st.session_state["lista_alimentos"])
-    st.session_state["lista_alimentos"] = st.data_editor(st.session_state["lista_alimentos"])
+        st.write("Lista de alimentos")
+        #st.write(st.session_state["lista_alimentos"])
+        st.session_state["lista_alimentos"] = st.data_editor(st.session_state["lista_alimentos"])
+
+    with tab_2:
+        st.write("Outro Alimentio")
 
     st.divider()
 
@@ -105,6 +133,7 @@ def main():
         st.session_state["equivalencia_hc_insulina"],
         st.session_state["fator_sensibilidade"],
         st.session_state["glicemia"],
+        st.session_state["glicemia_alvo"],
         st.session_state["tendencia_glicemia"],
         st.session_state["lista_alimentos"],
     )
