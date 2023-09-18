2d48a75275cb866604617503d42441a9e1befaf4
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-09-18 12:27:07 +0100

    UI UX upgrade

--- main.py
+++ main.py
@@ -3,41 +3,23 @@ import pandas as pd
 
 
 def load_data():
-    """# read csv
-    df_insa = pd.read_excel("data/insa_tca.xlsx")
-
-    # remove 2nd row a header
-    df_insa.columns = df_insa.iloc[0]
-
-    # remove 1st row
-    df_insa = df_insa.iloc[1:]
-
-    # get the column nname with the HC (it has some special characters)
-    coluna_hc = df_insa.columns[13]
-
-    # create a new column with the name of the food and the HC per 100g
-    df_insa["alimento_hc"] = df_insa.apply(lambda row: f"{row['Nome do alimento']} ({row[coluna_hc]}g HC/100g)", axis=1)
-
-    return df_insa
-    """
     return pd.read_csv("data/insa_tca_processed.csv")
 
-def formula_insulina(equivalencia_hc_insulina, fator_sensibilidade, alimento):
+def formula_insulina_hc(equivalencia_hc_insulina, hc):
 
-    df_insa = load_data()
+    insulina_para_correcao = (hc / equivalencia_hc_insulina)
 
-    hc_por_100 = df_insa[df_insa["alimento_hc"] == alimento["Alimento"]][df_insa.columns[13]].values[0]
-
-    hc = alimento["Quantidade"]*hc_por_100/100
-
-    return hc
+    return insulina_para_correcao
 
 
 def correcao_glicemia(fator_sensibilidade, glicemia, glicemia_alvo):
-    return (glicemia - glicemia_alvo) / fator_sensibilidade
+
+    insulina_para_correcao = (glicemia - glicemia_alvo) / fator_sensibilidade
+
+    return insulina_para_correcao
 
 
-def calculo_insulina(equivalencia_hc_insulina, fator_sensibilidade, glicemia, glicemia_alvo, tendencia_glicemia, lista_alimentos):
+def calculo_insulina(equivalencia_hc_insulina, fator_sensibilidade, glicemia, glicemia_alvo, tendencia_glicemia, hidratos_carbono):
 
     # insulin dose reset
     dose_insulina = 0
@@ -47,87 +29,118 @@ def calculo_insulina(equivalencia_hc_insulina, fator_sensibilidade, glicemia, gl
 
     dose_insulina += correcao_glicemia(fator_sensibilidade, glicemia, glicemia_alvo)
 
-    # check if there are foods in the list
-    if len(lista_alimentos) != 0:
-        # loop through the list of foods
-        for alimento in lista_alimentos:
-            # get the amount of insulin for each food
-            dose_insulina += formula_insulina(equivalencia_hc_insulina, fator_sensibilidade, alimento)
+    dose_insulina += formula_insulina_hc(equivalencia_hc_insulina, hidratos_carbono)
 
     # add the correction dose according to the glycemia and the trend
-
-    if tendencia_glicemia == "ascendete inclinado":
+    if tendencia_glicemia == "⏫":
         dose_insulina *= 1.2
-    elif tendencia_glicemia == "ascendente":
+    elif tendencia_glicemia == "↗️":
         dose_insulina *= 1.1
-    elif tendencia_glicemia == "descendente inclinado":
+    elif tendencia_glicemia == "⏬":
         dose_insulina *= 0.8
-    elif tendencia_glicemia == "descendente":
+    elif tendencia_glicemia == "↘️":
         dose_insulina *= 0.9
+
+    # round to 1 decimal (maybe 0 is better)
+    dose_insulina = round(dose_insulina, 1)
 
     return dose_insulina
 
+def calculo_hc(lista_alimentos):
+    hc = 0
+    for alimento in lista_alimentos:
+        df_insa = load_data()
+
+        hc_por_100 = df_insa[df_insa["alimento_hc"] == alimento["Alimento"]][df_insa.columns[13]].values[0]
+
+        hc += alimento["Quantidade"] * hc_por_100 / 100
+
+    return hc
 
 def main():
-    st.title("Calculadora Insulina")
+    st.title("Calculadora de Dose de Insulina")
+    st.markdown("##### Ferramenta de apoio ao :blue[cálculo da dose de insulina] a administrar para pessoas com diabetes.")
+    st.write("""
+        Permite:
+        - Correção de glicémia atual
+        - Correção de glicémia atual + Calculo para ingestão de alimentos utizanndo: 
+            - hidratos de carbono ingerido OU
+            - cálculo de hidratos de carbono a partir de uma lista de alimentos
+        """)
 
-    if "lista_alimentos" not in st.session_state:
-        st.session_state["lista_alimentos"] = []
 
-    if "dose_insulina" not in st.session_state:
-        st.session_state["dose_insulina"] = 0
+    if "lista_alimentos" not in st.session_state: st.session_state["lista_alimentos"] = []
+    if "dose_insulina" not in st.session_state: st.session_state["dose_insulina"] = 0
+    if "equivalencia_hc_insulina" not in st.session_state: st.session_state["equivalencia_hc_insulina"] = 12
+    if "fator_sensibilidade" not in st.session_state: st.session_state["fator_sensibilidade"] = 50
+    if "glicemia" not in st.session_state: st.session_state["glicemia"] = 200
+    if "glicemia_alvo" not in st.session_state: st.session_state["glicemia_alvo"] = 100
+    if "tendencia_glicemia" not in st.session_state: st.session_state["tendencia_glicemia"] = "↔️"
+    if "alimento" not in st.session_state: st.session_state["alimento"] = ""
+    if "quantidade" not in st.session_state: st.session_state["quantidade"] = 0
+    if "hc" not in st.session_state: st.session_state["hc"] = 0
 
     df_insa = load_data()
-
-
     # lista de alimentos
     lista_alimentos = df_insa["alimento_hc"]
-
     #st.table(df_insa[["Nome do alimento","Hidratos de carbono [g]"]].head(10))
 
-    col_1, col_2, col_3, col_4, col_5 = st.columns(5)
+    st.divider()
+    st.header("Correção de glicémia")
 
+    col_1, col_2, col_3 = st.columns(3)
     with col_1:
-        st.session_state["equivalencia_hc_insulina"] = st.number_input("Equivalencia HC", value=12, step=1, min_value=10, max_value=15)
+        st.session_state['glicemia'] = st.number_input("Glicémia Actual (mg/dL)", value=200, step=5)
     with col_2:
-        st.session_state["fator_sensibilidade"] = st.number_input("Fator Sensibilidade", value=30, step=5)
+        st.session_state["glicemia_alvo"] = st.number_input("Glicémia Alvo (mg/dL)", value=100, step=5)
+
     with col_3:
-        st.session_state['glicemia'] = st.number_input("Glicémia (mg/dl)", value=100, step=5)
-    with col_4:
-        st.session_state["glicemia_alvo"] = st.number_input("Glicémia Alvo (mg/dl)", value=100, step=5)
-    with col_5:
-        st.session_state["tendencia_glicemia"] = st.radio("Tendencia Glicemia", ["estável", "ascendente", "ascendete inclinado", "descendente", "descendente inclinado"], horizontal=True)
+        st.session_state["fator_sensibilidade"] = st.number_input("Fator Sensibilidade", value=50, step=5)
+    st.session_state["tendencia_glicemia"] = st.radio("Tendencia da Glicemia", ["⏫", "↗️", "↔️", "↘️", "⏬"], horizontal=True, index=2)
+
+    st.divider()
+    st.title("Alimentos")
+    if st.checkbox("Incluir Alimentos no Cálculo", value=False):
+        with st.container():
+            st.session_state["equivalencia_hc_insulina"] = st.number_input("Equivalencia HC", value=12, step=1)
+
+            tab_1, tab_2 = st.tabs(["Hidratos Carbono", "Lista de alimentos"])
+
+            with tab_1:
+                st.session_state["hc"] = st.number_input("Hidratos de Carbono (g)", value=0, step=1)
+
+            with tab_2:
+
+                col_5, col_6 = st.columns(2)
+
+                with col_5:
+                    st.session_state["alimento"] = st.selectbox("Alimento", lista_alimentos, index=0)
+                with col_6:
+                    st.session_state["quantidade"] = st.number_input("Quantidade (g)", value=50, step=5)
+                col_but_1, col_but_2 = st.columns(2)
+                with col_but_1:
+                    if st.button("adicionar alimento", type="secondary"):
+                        st.session_state["lista_alimentos"].append(
+                            {
+                                "Alimento": st.session_state["alimento"],
+                                "Quantidade": st.session_state["quantidade"],
+                            }
+                        )
+                with col_but_2:
+                    if st.button("limpar lista", type="primary"):
+                        st.session_state["lista_alimentos"] = []
+
+                st.write("Lista de alimentos")
+                if len(st.session_state["lista_alimentos"]) != 0:
+                    st.session_state["lista_alimentos"] = st.data_editor(st.session_state["lista_alimentos"])
+                    st.session_state["hc"] = calculo_hc(st.session_state["lista_alimentos"])
+    else:
+        # reset hc se não existir alimentos (checkbox desmarcado)
+        st.session_state["hc"] = 0
 
     st.divider()
 
-    tab_1, tab_2 = st.tabs(["Lista de alimentos", "outros"])
-    with tab_1:
-        col_5, col_6= st.columns(2)
-
-        with col_5:
-            st.session_state["alimento"] = st.selectbox("Alimento", lista_alimentos, index=0)
-        with col_6:
-            st.session_state["quantidade"] = st.number_input("Quantidade (g)", value=50, step=5)
-
-        if st.button("adicionar alimento", type="secondary"):
-            st.session_state["lista_alimentos"].append(
-                {
-                    "Alimento": st.session_state["alimento"],
-                    "Quantidade":st.session_state["quantidade"]
-                }
-            )
-
-        if st.button("limpar lista", type="primary"):
-            st.session_state["lista_alimentos"] = []
-
-        st.write("Lista de alimentos")
-        #st.write(st.session_state["lista_alimentos"])
-        st.session_state["lista_alimentos"] = st.data_editor(st.session_state["lista_alimentos"])
-
-    with tab_2:
-        st.write("Outro Alimentio")
-
-    st.divider()
+    st.title("Dose de Insulina")
 
     insulina = calculo_insulina(
         st.session_state["equivalencia_hc_insulina"],
@@ -135,16 +148,28 @@ def main():
         st.session_state["glicemia"],
         st.session_state["glicemia_alvo"],
         st.session_state["tendencia_glicemia"],
-        st.session_state["lista_alimentos"],
+        st.session_state["hc"],
     )
 
 
+    st.metric("Dose Insulina", insulina,)
 
-    st.metric("Dose Insulina", insulina)
     with st.expander("Detalhes calculo"):
         st.write("""
             The dose of insulin is calculated by summing the amount of insulin for each food and adding the correction dose according to the glycemia and the trend.
             """)
+
+    st.divider()
+
+    st.info("""
+        ### Definições
+        - **Glicémia Actual**: Glicémia medida no momento, por tira ou sensor
+        - **Glicémia Alvo**: Glicémia que se pretende atingir
+        - **Fator Sensibilidade**: Quantidade de mg/dL que a glicémia desce com 1 unidade de insulina. É variável de pessoa para pessoa, e traduz a resistência à insulina. Deve ser calculado pelo médico
+        - **Tendencia da Glicemia**: Tendencia da glicémia (medida pelo sensor), pode estar estável, a subir, a descer, a subir rapidamente ou a descer rapidamente
+        - **Equivalencia HC**: Quantidade (gramas) de hidratos de carbono que 1 unidade de insulina consegue metabolizar
+        """)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
