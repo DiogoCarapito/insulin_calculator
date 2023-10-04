91908aa17af42b4fbc0e785b483b6a716ee4d1e3
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-10-04 20:06:59 +0000

    data/insa_lista_alimentos_hidratos.csv

--- data/insa_tca_preprocess.py
+++ data/insa_tca_preprocess.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# read csv
+
 df_insa = pd.read_excel("insa_tca.xlsx")
 
 # remove 2nd row a header
@@ -17,4 +17,13 @@ df_insa["alimento_hc"] = df_insa.apply(
     lambda row: f"{row['Nome do alimento']} ({row[coluna_hc]}g HC/100g)", axis=1
 )
 
+# save the dataframe to a csv file
 df_insa.to_csv("insa_tca_processed.csv", index=False)
+
+# create a new dataframe with the food name and the HC per 100g
+df_insa_hc = df_insa[["Nome do alimento", 'Hidratos de carbono \n[g]']]
+
+#print(df_insa.columns)
+
+# Save the dataframe to a csv file
+df_insa_hc.to_csv("insa_lista_alimentos_hidratos.csv", index=False)
